3245097a7026497a5e3f24b123f03f3b0c7a946d
nicholassitoRMIT <s3951974@student.rmit.edu.au>
2023-10-19 21:11:44 +1100

    added tests

--- src/test/java/IssueBook.java
+++ src/test/java/IssueBook.java
@@ -1,11 +1,15 @@
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.ResourceLock;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import java.util.Date;
+import java.time.LocalDate; // changed to localdate
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 // importing array list function 
 import java.util.ArrayList;
 
@@ -18,9 +22,12 @@ import java.util.ArrayList;
  * Initialize the test object with "setting" method.
  */
 public class IssueBook {
-	@BeforeEach
-    public void setUp() {
-        // adding 4 books to the book list, should be valid
+    @Test
+    @DisplayName("Should return failure if actual number of books is different from expected value")
+    void bookNumberTest() {
+        int expected = 4;
+        int actual = 0;
+
         ArrayList<Book> testBookList = new ArrayList<Book>();
 
         Book book1 = new Book(1, "book1", 0);
@@ -33,14 +40,41 @@ public class IssueBook {
         testBookList.add(book3);
         testBookList.add(book4);
 
-        for (int i = 0; i < 4; i++) {
-            System.out.println(testBookList.get(i).getTitle());
+        // To get actual number of books
+        for (int i = 0; i < testBookList.size(); i++) {
+            actual++;
+        }
+
+        assertEquals(expected, actual, "List should not be more than 4 items in size");
+    }
+
+    @Test
+    @DisplayName("Should return custom exception if same book is issued")
+    void exceptionTest() {
+        Book book1 = new Book(1, "book1", 0);
+        Book book2 = new Book(2, "book1", 0);
+
+        try {
+            if (book1.getTitle().equalsIgnoreCase(book2.getTitle())) {
+                throw new IllegalBookIssueException("Issue Error: The book has already been issued on the library card.");
+            }
+        } catch (IllegalBookIssueException e) {
+            System.out.println(e);
         }
     }
-	
+
     @Test
-    @DisplayName("Test for book number validity")
-    public void bookNumberTest() {
-        System.out.println("test");
+    @DisplayName("Should return failure if library card is not valid")
+    void libraryCardValidityTest() {
+        LocalDate currentDate = LocalDate.now();
+        LocalDate cardDate = currentDate.minusDays(1);
+        boolean cardValidity = true; // card is assumed to be true at the start
+
+        // Card should be invalid if date is the same
+        if ((currentDate.compareTo(cardDate) == 0) || (currentDate.compareTo(cardDate) > 0)){
+            cardValidity = false;
+        }
+
+        assertEquals((currentDate.equals(cardDate)), cardValidity, "Only earlier dates than the actual date should be valid");
     }
 }
